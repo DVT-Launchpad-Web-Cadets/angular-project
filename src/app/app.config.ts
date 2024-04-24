2cc6757d42957aee6771da953d1327ae1f1a2835
Megan Hugo <mhugo@dvtsoftware.com>
2024-04-24 11:00:21 +0200

    feat: :heavy_plus_sign: Added ngrx

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,13 +5,14 @@ import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { routes } from './app.routes';
 import { environment } from '../environments/environment';
+import { provideStore } from '@ngrx/store';
 
 const firebaseConfig = environment.firebaseConfig;
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes), importProvidersFrom([
-    provideFirebaseApp(() => initializeApp(firebaseConfig)),
-    provideAuth(() => getAuth()),
-    provideFirestore(() => getFirestore())
-  ])]
+        provideFirebaseApp(() => initializeApp(firebaseConfig)),
+        provideAuth(() => getAuth()),
+        provideFirestore(() => getFirestore())
+    ]), provideStore()]
 };
