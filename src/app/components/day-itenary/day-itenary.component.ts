3a8ef5b1ec51b1bddd603baba7be42a4ac9451ff
Megan Hugo <mhugo@dvtsoftware.com>
2024-04-26 22:42:44 +0200

    feat: :sparkles: Added Day Itenary header

--- src/app/components/day-itenary/day-itenary.component.ts
+++ src/app/components/day-itenary/day-itenary.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { TagComponent } from "../shared/tag/tag.component";
-import { matAirplanemodeActive, matShoppingBag, matHotel, matRestaurant, matCameraAlt, matDirectionsRun, matLocalFlorist, matCoffee, matTrain, matQuestionMark } from '@ng-icons/material-icons/baseline';
+import { matAirplanemodeActive, matShoppingBag, matHotel, matRestaurant, matCameraAlt, matDirectionsRun, matLocalFlorist, matCoffee, matTrain, matQuestionMark, matEdit, matClose, matLocationOn } from '@ng-icons/material-icons/baseline';
 import { NgIconComponent, provideIcons } from '@ng-icons/core';
 
 @Component({
@@ -9,9 +9,11 @@ import { NgIconComponent, provideIcons } from '@ng-icons/core';
     templateUrl: './day-itenary.component.html',
     styleUrl: './day-itenary.component.css',
     imports: [TagComponent, NgIconComponent],
-    viewProviders: [provideIcons({ matAirplanemodeActive, matShoppingBag, matHotel, matRestaurant, matCameraAlt, matDirectionsRun, matLocalFlorist, matCoffee, matTrain, matQuestionMark })]
+    viewProviders: [provideIcons({ matAirplanemodeActive, matShoppingBag, matHotel, matRestaurant, matCameraAlt, matDirectionsRun, matLocalFlorist, matCoffee, matTrain, matQuestionMark, matEdit, matClose, matLocationOn })]
 
 })
 export class DayItenaryComponent {
-
+    date = 'Tuesday, 03 January';
+    location = 'Add Location';
+    cost = 'R2 590';
 }
