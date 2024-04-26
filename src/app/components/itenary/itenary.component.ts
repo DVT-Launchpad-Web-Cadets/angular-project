fe183adc57f8669a8a0df004b0b4c6faac2c2122
Megan Hugo <mhugo@dvtsoftware.com>
2024-04-26 14:04:01 +0200

    feat: :sparkles: Added Itenary Layout

--- src/app/components/itenary/itenary.component.ts
+++ src/app/components/itenary/itenary.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { ItenaryService } from '../../services/itenary.service';
 import { ItenaryFirebaseService } from '../../services/itenary-firebase.service';
+import { DayComponent } from "./day/day.component";
 
 @Component({
-  selector: 'app-itenary',
-  standalone: true,
-  imports: [],
-  templateUrl: './itenary.component.html',
-  styleUrl: './itenary.component.css'
+    selector: 'app-itenary',
+    standalone: true,
+    templateUrl: './itenary.component.html',
+    styleUrl: './itenary.component.css',
+    imports: [DayComponent]
 })
 export class ItenaryComponent implements OnInit {
   itenaryService = inject(ItenaryService);
--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NzTabsModule } from 'ng-zorro-antd/tabs';
 import { NzButtonModule } from 'ng-zorro-antd/button';
+import { ItenaryComponent } from '../itenary/itenary.component';
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [NzTabsModule, NzButtonModule],
+  imports: [NzTabsModule, NzButtonModule, ItenaryComponent],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
