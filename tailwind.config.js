afe50033ce1c1bccd53da9dca184ebf610506c3b
Megan Hugo <mhugo@dvtsoftware.com>
2024-04-29 10:15:38 +0200

    feat: :heavy_plus_sign: Added DaisyUI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
+  content: ["./src/**/*.{html,ts}"],
   theme: {
     colors: {
-      'dark-blue': '#006D77',
-      'light-blue': '#83C5BE',
-      'coral': '#E29578',
-      'cream': '#EDF6F9',
-      'white': '#FFFFFF',
-      'black': '#000000',
-      'gray': '#E0DEDE',
-      'dark-gray': '#A5A5A5',
+      "dark-blue": "#006D77",
+      "light-blue": "#83C5BE",
+      coral: "#E29578",
+      cream: "#EDF6F9",
+      white: "#FFFFFF",
+      black: "#000000",
+      gray: "#E0DEDE",
+      "dark-gray": "#A5A5A5",
     },
     extend: {},
   },
-  plugins: [],
-}
-
+  plugins: [require("daisyui")],
+};
