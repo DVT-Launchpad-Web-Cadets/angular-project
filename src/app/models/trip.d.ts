cdd287687bc94e8b3acf1d01e0896119347aa899
Megan Hugo <mhugo@dvtsoftware.com>
2024-05-03 22:44:41 +0200

    fix: :bug: Changed trip interface

--- src/app/models/trip.d.ts
+++ src/app/models/trip.d.ts
@@ -1,8 +1,8 @@
 export interface TripInterface {
-    id: number;
-    userId: number;
-    name: string;
-    location: string;
-    startDate: string;
-    endDate: string;
+  id: number;
+  userId: number;
+  name: string;
+  destination: string;
+  startDate: string;
+  endDate: string;
 }
