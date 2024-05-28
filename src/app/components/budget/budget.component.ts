b877428bc5f3d507ed4bebb868e1e9fd588e4398
Megan Hugo <mhugo@dvtsoftware.com>
2024-05-28 07:30:18 +0200

    feat: :fire: Cleaned up budget code

--- src/app/components/budget/budget.component.ts
+++ src/app/components/budget/budget.component.ts
@@ -7,22 +7,11 @@ import {
 } from '@ng-icons/material-icons/baseline';
 import { NgIconComponent, provideIcons } from '@ng-icons/core';
 import {
-  Observable,
   Subscription,
-  from,
-  groupBy,
-  map,
-  mergeMap,
-  reduce,
-  scan,
-  startWith,
-  tap,
-  toArray,
 } from 'rxjs';
 import { EventState, TripState } from '../../store/state';
 import { Store } from '@ngrx/store';
-import { selectCurrencyInfo, selectEvents, selectTagTotals } from '../../store/selectors';
-import { EventInterface, TagType } from '../../models';
+import { selectCurrencyInfo, selectTagTotals, selectTotalCost } from '../../store/selectors';
 import { AsyncPipe, DecimalPipe } from '@angular/common';
 
 @Component({
@@ -42,24 +31,15 @@ import { AsyncPipe, DecimalPipe } from '@angular/common';
 export class BudgetComponent {
   totalCost = 0;
   currency = '';
-
   homeCurrency = '';
   currencySymbol = '';
   conversionRate = 1;
 
-  budgetItems$: Observable<{ tag: TagType; totalCost: number }> | undefined;
-
-  array : { tag: TagType; totalCost: number }[] = [];
-  eventss: EventInterface[] = [];
-
-  events$ = this.eventStore.select(selectEvents);
   currencyInfo$ = this.tripStore.select(selectCurrencyInfo);
-
+  totalCost$ = this.eventStore.select(selectTotalCost);
   tagTotals$ = this.eventStore.select(selectTagTotals);
 
   subscription: Subscription | undefined;
-
-  totalCost$: Observable<number> | undefined;
 
   constructor(
     private eventStore: Store<EventState>,
@@ -67,108 +47,22 @@ export class BudgetComponent {
   ) {}
 
   ngOnInit(): void {
-    this.currencyInfo$.subscribe((currencyInfo) => {
+    this.subscription = this.currencyInfo$.subscribe((currencyInfo) => {
       this.currency = currencyInfo.homeCurrency ?? this.currency;
       this.homeCurrency = currencyInfo.homeCurrency ?? this.currency;
-      this.conversionRate = currencyInfo.exchangeRate ?? this.conversionRate;
       this.currencySymbol = currencyInfo.homeCurrencySymbol ?? this.currencySymbol;
-    });
-
-    this.totalCost$ = this.events$.pipe(
-      map((events) =>
-        events.reduce((total, event) => {
-          if (event.currency === this.homeCurrency) {
-            return total + event.cost;
-          } else return total + event.cost / this.conversionRate;
-        }, 0)
-      )
-    );
-
-    // for later use
-
-    this.budgetItems$ = this.events$.pipe(
-      mergeMap((events) => from(events)),
-      groupBy((event) => event.tag),
-      mergeMap((group$) =>
-        group$.pipe(
-          map((event) =>
-            event.currency === this.homeCurrency
-              ? event.cost
-              : event.cost / this.conversionRate
-          ),
-          startWith(0),
-          scan((sum, cost) => sum + cost),
-          map((totalCost) => ({ tag: group$.key, totalCost: totalCost as number })) // Cast totalCost to number
-        )
-      ),
-      tap((item) => {
-        this.replaceBudgetItem(item);
-        console.log('Item:', item);
-        console.log('Array:', this.array);
-      })
-    );
-    // why does this only return a single item? Not a whole array?
-    // this.budgetItems$ = this.events$.pipe(
-    //   mergeMap((events) => from(events)),
-    //   groupBy((event) => {
-    //     return event.tag;
-    //   }),
-    //   mergeMap((group$) => {
-    //     console.log('Group:', group$);
-    //     return group$.pipe(
-    //       map((event) => {
-    //         if (event.currency === this.homeCurrency) {
-    //           return event.cost;
-    //         } else {
-    //             return event.cost / this.conversionRate
-    //         };
-    //       }),
-    //       scan((sum, eventCost) => {
-    //         return sum + eventCost;
-    //       }, 0),
-    //       map((totalCost) => {
-    //         return { tag: group$.key, totalCost };
-    //       })
-    //     );
-    //   }),
-    //   tap((item) => {
-    //     this.array.push(item);
-    //     console.log('Item:', item);
-    //     console.log('Array:', this.array);
-    //   }),
-    //   map((item) => [item]), // Wrap the object in an array
-    //   tap((itemsArray) => console.log('Final Result:', itemsArray))
-    // );
-
-
-
-    this.events$.subscribe((events) => {
-      this.eventss = events;
     });
   }
 
-  setCurrency(currency: string, symbol: string): void {
+  setCurrency(currency: string, symbol: string, exchangeRate: number): void {
     this.currency = currency;
     this.currencySymbol = symbol;
+    this.conversionRate = exchangeRate;
   }
 
   ngOnDestroy(): void {
     if (this.subscription) {
       this.subscription.unsubscribe();
-    }
-  }
-
-  replaceBudgetItem(item: { tag: TagType; totalCost: number }) {
-    const existingIndex = this.array.findIndex(
-      (arrItem) => arrItem.tag === item.tag
-    );
-  
-    if (existingIndex === -1) {
-      // If the tag doesn't exist in the array, push the new item
-      this.array.push(item);
-    } else {
-      // If the tag exists, replace the existing item with the new one
-      this.array[existingIndex] = item;
     }
   }
 }
